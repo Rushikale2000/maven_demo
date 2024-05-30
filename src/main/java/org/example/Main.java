68d4859bbe20b01ca378c847c2730b51017acad3
Rushikale2000 <rushikesh.2.kale@eviden.com>
2024-05-30 16:24:55 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Webhook updated");
-        System.out.println("Webhook updated again");
+        System.out.println("WebHook updated");
+        System.out.println("WebHook updated again");
+        System.out.println("WebHook updated again");
     }
 }
